7b5fd5fdbf4827a062d8de2b49c52d6b4d145117
Yashdarji77 <yashdarji2403@gmail.com>
2024-11-01 11:35:01 +0530

    Hotel

--- pages/api/hotels/index.js
+++ pages/api/hotels/index.js
@@ -2,35 +2,19 @@ import connectDB from "@/db";
 import Hotel from "@/models/hotel-model";
 
 export default async function handler(req, res) {
-  try {
-    await connectDB();
+  connectDB();
+  // if (req.method === "POST") {
+  //   const newHotel = new Hotel(req.body);
+  //   const result = await newHotel.save();
+  //   res.status(201).json({ message: "Hotel Added!", result });
+  // }
 
-    // if (req.method === "POST") {
-    //   const newHotel = new Hotel(req.body);
-    //   const result = await newHotel.save();
-    //   res.status(201).json({ message: "Hotel Added!", result });
-    // }
-
-    if (req.method === "GET") {
-      const hotels = req.query.city
-        ? await Hotel.find({ location: req.query.city })
-        : await Hotel.find({});
-
-      if (hotels.length > 0) {
-        return res.status(200).json({ message: "Success", hotels });
-      } else {
-        return res
-          .status(404)
-          .json({ message: "No hotels found for the specified city." });
-      }
+  if (req.method === "GET") {
+    const hotels = await Hotel.find({ location: req.query.city });
+    if (hotels.length > 0) {
+      return res.status(200).json({ message: "Good", hotels });
     }
-
-    res.setHeader("Allow", ["GET"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  } catch (error) {
-    console.error("Error fetching hotels:", error.message);
-    res
-      .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
+    const allhotels = await Hotel.find({});
+    return res.status(200).json({ message: "Good", allhotels });
   }
 }
